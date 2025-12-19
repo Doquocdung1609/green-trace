7c045fc240476e8c0ed791111368655f267d1412
Doquocdung1609 <doquocdung1609@gmail.com>
2025-12-20 01:06:42 +0700

    fix error

--- src/contexts/WalletContext.tsx
+++ src/contexts/WalletContext.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
-const network = WalletAdapterNetwork.Devnet; // Change to Mainnet for production
-const endpoint = 'https://api.devnet.solana.com'; // Solana RPC
+const endpoint = 'https://api.devnet.solana.com';
 
 const wallets = [new PhantomWalletAdapter()];
 
